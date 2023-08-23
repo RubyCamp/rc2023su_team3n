b9ac21fdb69c82b571c036ee6a7d6587317c4eeb
Kazuki Yamasaki <take10real@gmail.com>
2023-08-23 14:27:49 +0900

    追加

--- tako.rb
+++ tako.rb
@@ -1,5 +1,6 @@
 #dxruby  画面表示
 require 'dxruby'
+z = 0 #ルート取るための変数
 Window.bgcolor = C_WHITE
 @img_color = C_BLACK
 img_waku = C_BLACK
@@ -24,11 +25,73 @@ class Waku
     end
 end
 Window.loop do
-    if img_waku == C_BLACK then #黒枠を表示するための処理
+if z < 2 then 
+    z += 1
+    
+    
+if img_waku == C_BLACK then #黒枠を表示するための処理
+    z += 1
+    if z < 2 then #座標ｙ軸を下にする。
+       @b -= 100
+
+    end 
+    if z == 2 then#ｘ軸を右にする。
+        @a += 100
+
+    end
+    if z < 5 && z > 2  then#y軸を上にする。
+        @b += 100
+
+    end
+    if z == 5  then#x軸を右にする。
+        @a += 100
+     
+    end
+    if z < 8 && z > 5 then#x軸を下にする
+        @b -= 100
+     
+    end
+    if z == 8 then#x軸を右にする
+        @a += 100
+    end
+
+    if z < 11 && z>8 then#y軸を上にする。
+        @b += 100
+     
+    end
+      
     test1 = Waku.new(100,100)
     test1.update
-end    
+    z += 1
+end  
 if @img_color != C_BLACK then #中の色を表示するための処理
+    if z < 2 then
+        @b -= 100
+    end
+
+    if z  == 2 then
+        @a += 100
+    end
+
+    if z  < 5 && z > 2 then
+        @a += 100
+    end
+
+    if z == 5  then
+        @a += 100
+    end
+
+    if z < 8  && z > 5 then
+        @b -= 100
+    end
+
+    if z == 8 then
+        @a += 100
+    end
+
+    if z < 11 && z > 8 then   
+        @a += 100
+    end
     test2 = Hose.new(100,100,Image.new(90,90,@img_color))
     test2.update
 end
