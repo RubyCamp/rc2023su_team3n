c1125154c5f4b6cb211d1c1cfe47a7bc98d469fe
fuzi1123 <toa0306@icloud.com>
2023-08-23 09:37:49 +0900

    'lt

--- dxruby.rb
+++ dxruby.rb
@@ -6,6 +6,8 @@ img_red   = Image.new(90,90,C_BLUE)
 img_green = Image.new(90,90,C_GREEN)
 img_white = Image.new(90,90,C_WHITE)
 img_blue  = Image.new(90,90,C_BLUE)
+map_color    #外枠に入った時の変数
+map_status   #送られてきたマップの色を使用するための変数
 
 class Hose #インスタンス変数の初期化や設定などを行う
     def initialize(x,y,img_color)
@@ -19,7 +21,8 @@ class Hose #インスタンス変数の初期化や設定などを行う
     end
 end
 Window.loop do #画像を表示するための処理
-@img_color = C_GREEN
-test = Hose.new(100,100,Image.new(90,90,@img_color))
+if map_color == C_BLACK #黒のラインに入ったときにマップを生成
+    @img_color = map_status
+    test = Hose.new(100,100,Image.new(90,90,@img_color))
 test.update
 end
