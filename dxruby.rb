7d2b8d35fdf4d1c6d0f9e7ed63a0e31af64e5e18
fuzi1123 <toa0306@icloud.com>
2023-08-23 13:21:15 +0900

    'dxruby'

--- dxruby.rb
+++ dxruby.rb
@@ -1,28 +1,35 @@
 #dxruby  画面表示
 require 'dxruby'
 Window.bgcolor = C_WHITE
-img_waku  = Image.new(100,100,C_BLACK)
-img_red   = Image.new(90,90,C_BLUE)
-img_green = Image.new(90,90,C_GREEN)
-img_white = Image.new(90,90,C_WHITE)
-img_blue  = Image.new(90,90,C_BLUE)
-map_color    #外枠に入った時の変数
-map_status   #送られてきたマップの色を使用するための変数
-
+@img_color = C_BLACK
+img_waku = C_BLACK
+color_number = [C_BLACK,C_BLUE,C_GREEN,C_RED,C_WHITE]#色の配列
 class Hose #インスタンス変数の初期化や設定などを行う
     def initialize(x,y,img_color)
         @x = x
         @y = y
-        @img_color = img_color
+        @img_color = img_color   #黒枠の中の色を確かめるための変数
     end
-    def update #座標と色のアップデート
-        Window.draw(@x,@y,Image.new(100,100,C_BLACK))
+    def update() #座標と色のアップデート
         Window.draw(@x+5,@y+5,@img_color)
     end
 end
-Window.loop do #画像を表示するための処理
-if map_color == C_BLACK #黒のラインに入ったときにマップを生成
-    @img_color = map_status
-    test = Hose.new(100,100,Image.new(90,90,@img_color))
-test.update
+class Waku 
+    def initialize(a,b)
+        @a = a
+        @b = b
+    end
+    def update()
+        Window.draw(@a,@b,Image.new(100,100,C_BLACK))
+    end
+end
+Window.loop do
+    if img_waku == C_BLACK then #黒枠を表示するための処理
+    test1 = Waku.new(100,100)
+    test1.update
+end    
+if @img_color != C_BLACK then #中の色を表示するための処理
+    test2 = Hose.new(100,100,Image.new(90,90,@img_color))
+    test2.update
+end
 end
